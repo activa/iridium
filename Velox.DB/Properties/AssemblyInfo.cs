e3045b229e617fe4d64538e1ddf2955eda9abd2d
Philippe Leybaert <philippe@axum.be>
2015-04-14 22:14:01 -0500

    0.9.19 release

--- Velox.DB/Properties/AssemblyInfo.cs
+++ Velox.DB/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.9.18.*")]
-[assembly: AssemblyInformationalVersion("0.9.18")]
+[assembly: AssemblyVersion("0.9.19.*")]
+[assembly: AssemblyInformationalVersion("0.9.19")]
 
 [assembly: InternalsVisibleTo("Velox.DB.TextExpressions")]
 
