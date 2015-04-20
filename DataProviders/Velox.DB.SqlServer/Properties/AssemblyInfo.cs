222898dc39a04883f04e879e831459b36cd21bb6
Philippe Leybaert <philippe@axum.be>
2015-04-20 17:40:53 -0500

    Build 0.9.33

--- DataProviders/Velox.DB.SqlServer/Properties/AssemblyInfo.cs
+++ DataProviders/Velox.DB.SqlServer/Properties/AssemblyInfo.cs
@@ -15,6 +15,6 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("73b73479-97b8-4233-88b4-93df05bdc5fd")]
 
-[assembly: AssemblyVersion("0.9.32.*")]
+[assembly: AssemblyVersion("0.9.33.*")]
 
-[assembly: AssemblyInformationalVersion("0.9.32")]
+[assembly: AssemblyInformationalVersion("0.9.33")]
--- Velox.DB/Properties/AssemblyInfo.cs
+++ Velox.DB/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.9.32.*")]
-[assembly: AssemblyInformationalVersion("0.9.32")]
+[assembly: AssemblyVersion("0.9.33.*")]
+[assembly: AssemblyInformationalVersion("0.9.33")]
 
 [assembly: InternalsVisibleTo("Velox.DB.TextExpressions")]
 
